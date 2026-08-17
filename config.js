cbe6b854779e266dfa3ec7459a71f9b291870d2c
nishantsingh2120 <nishantsingh63961@gmail.com>
2026-08-17 10:03:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase Configuration Object
 // Replace placeholders with your dynamic Supabase environment keys.
-const SUPABASE_URL = "https://kzutrwddzxpqjgfhhlhc.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://kzutrwddzxpqjgfhhlhc.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_3pQkQDCdjZgcABFzCVlGQA_NpivWNkl";
 
 // Maximum security violations allowed before automatic exam disqualification/submission
